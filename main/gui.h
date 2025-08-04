3da6fb1efba19f19216597109212aeb1d71552f1
ImLunchtime <knoxmedia@yeah.net>
2025-08-05 06:19:07 +0800

    revert

--- main/gui.h
+++ main/gui.h
@@ -1,14 +1,10 @@
 #pragma once
 
 #include "lvgl.h"
-#include "app_manager.h"
-#include "top_toolbar.h"
-#include "side_toolbar.h"
-#include "assets/assets.h"
 
 /**
- * @brief 初始化GUI界面
+ * @brief Initialize the GUI with a centered button
  * 
- * @param disp LVGL显示实例
+ * @param disp LVGL display instance
  */
-void gui_init(lv_disp_t *disp);
+void gui_init(lv_disp_t *disp); 
